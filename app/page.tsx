8ae89da3cece3be77a89374ede75031bdc56185f
Guiborgesilva <guiborgesilva@outlook.com>
2024-01-02 15:09:42 -0300

    Testes no design da tela inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
+import { Roboto } from 'next/font/google'
 
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: "400"
+})
 
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center mt-4 ml-auto mr-auto w-72">
-        <img className="logo" src="/logoSaraIcara.png" alt="Logo da Igreja Sara de Içara" />
+      <div className="flex justify-center items-center mt-20 flex-col gap-20">
+        <div>
+          <img src="/logoSaraIcara.png" alt="Logo da Igreja Sara de Içara" />
+        </div>
+        <div className="text-center text-white">
+          <p className="text-5xl">
+            PREPARADO PARA EXPERIMENTAR
+          </p>
+          <p className="text-5xl mt-5"><span className="text-6xl">O</span> SOBRENATURAL?
+          </p>
+        </div>
+        <button className="text-white w-[358px] h-[46px] rounded-[10px] bw">
+          <span className={roboto.className}>
+            QUERO ME INSCREVER!
+          </span>
+        </button>
       </div>
+
     </>
   )
 }
