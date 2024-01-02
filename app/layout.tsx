382367e97942d3d941b7f2edda2701889287be44
Guiborgesilva <guiborgesilva@outlook.com>
2024-01-02 15:37:53 -0300

    Teste toolbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ const Heydex = localFont({ src:'./Heydex.ttf' })
 export const metadata: Metadata = {
   title: 'Cadastro para o Revisão de Vidas',
   description: 'App para cadastro de pessoas para o Revisão de Vidas',
+  themeColor: '#000'
 }
 
 export default function RootLayout({
