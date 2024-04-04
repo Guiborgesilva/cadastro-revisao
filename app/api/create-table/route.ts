7a43859c5e2eb076540d885e2d99f6b53919189c
Guiborgesilva <guiborgesilva@outlook.com>
2024-04-04 11:58:24 -0300

    Resolvendo problema

--- app/api/create-table/route.ts
+++ app/api/create-table/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server'
 export async function GET(request: Request){
   try{
     const result =
-      await sql`CREATE TABLE vidas (id UUID DEFAULT uuid_generate_v4() PRIMARY KEY, nome_pessoa varchar(255), data_nascimento varchar(10), sexo varchar(50), lider_equipe varchar(100), telefone varchar(20), email varchar(50), nome_mae varchar(100), nome_pai varchar(100), nome_contato1 varchar(50), telefone_contato1 varchar(20), nome_contato2 varchar(50), telefone_contato2 varchar(20), nome_contato3 varchar(50), telefone_contato3 varchar(20), forma_pagamento varchar(50), created_at DATE NOT NULL)`
+      await sql`CREATE TABLE vidas (id UUID DEFAULT uuid_generate_v4() PRIMARY KEY, nome_pessoa varchar(255), data_nascimento varchar(10), sexo varchar(50), lider_equipe varchar(100), telefone varchar(20), email varchar(50), nome_mae varchar(100), nome_pai varchar(100), nome_contato1 varchar(50), telefone_contato1 varchar(20), nome_contato2 varchar(50), telefone_contato2 varchar(20), nome_contato3 varchar(50), telefone_contato3 varchar(20), forma_pagamento varchar(50), created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL)`
 
       return NextResponse.json({ result }, { status: 200 })
   } catch (error) {
