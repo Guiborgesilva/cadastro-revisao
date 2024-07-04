b10129f26dd39d4441972f1b45338d37a346f1ad
Guiborgesilva <guiborgesilva@outlook.com>
2024-07-04 11:22:24 -0300

    Altera a navbar no modo mobile

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,46 @@
 import SideNav from "@/app/ui/pages/Sidenav"
+import SideNavMobile from "@/app/ui/pages/SidenavMobile"
+import Image from "next/image"
+import Link from "next/link"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-full md:h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
+    <>
+      <div className="flex h-full md:h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="md:hidden px-3 py-4">
+          <Link
+            className="
+          mb-2
+          flex
+          h-20
+          items-center
+          justify-center
+          rounded-md
+          bg-black
+          p-4
+          md:h-40"
+            href="/">
+            <div className="w-32 text-white md:w-40">
+              <Image
+                width={260}
+                height={65}
+                src="/logoSaraIcara.png"
+                alt="Logo da Igreja Sara de Içara"
+                priority={true}
+              />
+            </div>
+          </Link>
+        </div>
+        <div className="w-full flex-none md:w-64 hidden">
+          <SideNav />
+        </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12 mb-24">
+          {children}
+        </div>
+        <div className="w-full flex-none md:w-64 fixed bottom-0 z-50">
+          <SideNavMobile />
+        </div>
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-    </div>
+    </>
   )
 }
---
+++ app/ui/pages/SidenavMobile.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link"
+import NavLinks from "@/app/ui/pages/NavLinks"
+import { Ripple } from "react-ripple-click"
+import { PowerIcon } from "@heroicons/react/24/outline"
+import { signOut } from "@/auth"
+import Image from "next/image"
+import Button from "@/app/ui/components/Buttons"
+
+export default function SideNavMobile() {
+  return (
+    <div className="flex h-full flex-col backdrop-blur-md px-3 py-4 md:px-2">
+      {/* <Link
+        className="
+          mb-2
+          flex
+          h-20
+          items-center
+          justify-center
+          rounded-md
+          bg-black
+          p-4
+          md:h-40"
+        href="/">
+        <div className="w-32 text-white md:w-40">
+          <Image
+            width={260}
+            height={65}
+            src="/logoSaraIcara.png"
+            alt="Logo da Igreja Sara de Içara"
+            priority={true}
+          />
+        </div>
+      </Link> */}
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 dark:bg-slate-700 md:block"></div>
+        <form
+          action={async () => {
+            "use server"
+            await signOut()
+          }}>
+          <Button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 dark:bg-slate-700 p-3 text-sm font-medium dark:hover:bg-blue-800 hover:bg-sky-100 hover:text-blue-600 dark:hover:text-white md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            <Ripple />
+            <div className="hidden md:block">Sign Out</div>
+          </Button>
+        </form>
+      </div>
+    </div>
+  )
+}
